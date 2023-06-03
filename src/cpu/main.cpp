7bf10e464a3cc8fcc420b161258fef1d48a29589
wuliJerry <wuliJerry927@outlook.com>
2023-06-03 10:12:25 +0800

    main.cpp

--- src/cpu/main.cpp
+++ src/cpu/main.cpp
@@ -2,10 +2,8 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
-#include <Eigen/Dense>
 
 
-// Input pattern M, K, N
 int main(int argc, char **argv) {
     assert(argc == 4);
 
@@ -13,7 +11,12 @@ int main(int argc, char **argv) {
            K = std::stol(argv[2]), 
            N = std::stol(argv[3]); 
 
+    int *mat1 = new int[M * K];
+    int *mat2 = new int[K * N];
+    int *matRst = new int[M * N];
 
+    delete [] mat1;
+    delete [] mat2;
 
     return 0;
 }
