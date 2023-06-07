547de5ef8f40c2726cb1a7535a87b42715a03332
wuliJerry <wuliJerry927@outlook.com>
2023-06-07 22:19:58 +0800

    change the CPU outer product

--- src/cpu/outer.cpp
+++ src/cpu/outer.cpp
@@ -1,14 +1,16 @@
+#include <cmath>
 #include <omp.h>
 #include <iostream>
 
 void matrixMultiply(int *mat1, int *mat2, int *matRst, size_t M, size_t K, size_t N) {
-#pragma omp parallel for
-	for (size_t i = 0; i < M; i++) {
-		for (size_t j = 0; j < N; j++) {
-			matRst[i*N + j] = 0;
-			for (size_t k = 0; k < K; k++) {
-				matRst[i*N + j] += mat1[i*K + k] * mat2[k*N + j];
-			}
-		}
-	}
+	
+    for (size_t k = 0; k < K; k++) {
+		#pragma omp parallel for
+        for (size_t i = 0; i < M; i++) {
+            for (size_t j = 0; j < N; j++) {
+                #pragma omp atomic
+                matRst[i*N + j] += mat1[i*K + k] * mat2[k*N + j];
+            }
+        }
+    }
 }
